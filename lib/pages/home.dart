2dd56b55300824ed9b04f40768554c3a71b63cfc
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-05-02 16:51:32 +0600

    directory ready for pet ui

---
+++ lib/pages/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  double xOffset = 0;
+  double yOffset = 0;
+  double scaleFactor = 1;
+
+  bool isDrawerOpen = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
